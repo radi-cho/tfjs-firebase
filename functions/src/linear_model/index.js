769d5e68d6e12cecc3fe1f21485c01920c77c1a1
Radi Cho <radicho123@gmail.com>
2018-09-06 21:35:42 +0300

    Refactor the linear model function.

--- functions/src/linear_model/index.js
+++ functions/src/linear_model/index.js
@@ -1,46 +1,56 @@
 const functions = require("firebase-functions");
-var tf = require("@tensorflow/tfjs");
 
-exports.runLinearModel = functions.https.onRequest(
-  async (request, response) => {
-    // Process the Firestore data
-    const xs_data = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
-    const ys_data = [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9];
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
-    // Define a model for linear regression.
-    const linearModel = tf.sequential();
-    linearModel.add(
-      tf.layers.dense({
-        units: 1,
-        inputShape: [1]
-      })
-    );
+const tf = require("@tensorflow/tfjs");
 
-    // Prepare the model for training: Specify the loss and the optimizer.
-    linearModel.compile({
-      loss: "meanSquaredError",
-      optimizer: "sgd"
+exports.runLinearModel = functions.https.onRequest((request, response) => {
+  // Get x_test value from the request body
+  const x_test = Number(request.body.x);
+
+  // Check if the x value is number. Otherwise request a valid one and terminate the function.
+  if (typeof x_test !== "number" || isNaN(x_test))
+    response.send("Error! Please format your request body.");
+
+  // Define a model for linear regression.
+  const linearModel = tf.sequential();
+  linearModel.add(
+    tf.layers.dense({
+      units: 1,
+      inputShape: [1]
+    })
+  );
+
+  // Prepare the model for training: Specify the loss and the optimizer.
+  linearModel.compile({
+    loss: "meanSquaredError",
+    optimizer: "sgd"
+  });
+
+  // Process the Firestore data
+  db.collection("linear-values")
+    .get()
+    .then(async querySnapshot => {
+      // Get tensor-like arrays from Firestore
+      const xs_data = querySnapshot.docs.map(doc => doc.data().x);
+      const ys_data = querySnapshot.docs.map(doc => doc.data().y);
+
+      // Train the model with those arrays
+      const xs = tf.tensor1d(xs_data);
+      const ys = tf.tensor1d(ys_data);
+      await linearModel.fit(xs, ys);
+
+      // Make a prediction
+      const result = await linearModel.predict(tf.tensor2d([x_test], [1, 1]));
+      const prediction = Array.from(result.dataSync())[0];
+
+      // Send the prediction back as a response
+      response.send(200, prediction);
+      return true;
+    })
+    .catch(e => {
+      response.send("Database error! " + e);
     });
-
-    // Training data, completely random stuff
-    const xs = tf.tensor1d(xs_data);
-    const ys = tf.tensor1d(ys_data);
-
-    // Train
-    await linearModel.fit(xs, ys);
-
-    // Get the x value from the request body
-    const x_test = Number(request.body.x);
-
-    // Check if the x value is number. Otherwise request a valid one and terminate the functions.
-    if (typeof x_test !== "number")
-      response.send("Error! Please format your request body.");
-
-    // Make a prediction
-    const result = await linearModel.predict(tf.tensor2d([x_test], [1, 1]));
-    const prediction = Array.from(result.dataSync())[0];
-
-    // Send the prediction back as a response
-    response.send(200, prediction);
-  }
-);
+});
