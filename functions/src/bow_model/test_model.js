2a4a2494a2ace9e88673338aae38546b6ab427d2
Radi Cho <radicho123@gmail.com>
2018-09-07 15:46:29 +0300

    Create testing method which triggers on model upload to google cloud storage.

--- functions/src/bow_model/test_model.js
+++ functions/src/bow_model/test_model.js
@@ -1,23 +1,40 @@
-var tf = require("@tensorflow/tfjs");
-require("@tensorflow/tfjs-node");
+const functions = require("firebase-functions");
 
-var fitData = require("./utils").fitData;
+const tf = require("@tensorflow/tfjs");
+require("tfjs-node-save");
+const fitData = require("./utils").fitData;
 
-async function predict(test_x) {
-  // This example is purposed only for testing
-  /// TODO: Use relative paths
-  var model = await tf.loadModel(
-    "file:///Projects/tensorflow/tfjs/test1/model/model.json"
-  );
+const { Storage } = require("@google-cloud/storage");
+const gcs = new Storage();
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
 
-  var score = model.predict(tf.tensor2d(test_x)).dataSync()[0];
-  var label = score < 0.5 ? "bad" : "good";
+exports.predict = functions.storage.object().onFinalize(async object => {
+  // const a = async object => {
+  const fileBucket = object.bucket;
+  const filePath = object.name;
+  const contentType = object.contentType;
+  const metageneration = object.metageneration;
+
+  const bucket = gcs.bucket(fileBucket);
+  const tempFilePath = path.join(os.tmpdir(), "model.json");
+  const tempFilePath2 = path.join(os.tmpdir(), "weights.bin");
+  const modelPath = "file:///" + path.join(os.tmpdir(), "model.json");
+
+  await bucket.file(filePath).download({ destination: tempFilePath });
+  await bucket.file("weights.bin").download({ destination: tempFilePath2 });
+
+  const model = await tf.loadModel(modelPath);
+
+  const test_x = [fitData("bad, really bad, ugly overrated")];
+  const score = model.predict(tf.tensor2d(test_x)).dataSync()[0];
+  const label = score < 0.5 ? "bad" : "good";
 
   console.log(score);
   console.log(label);
-}
 
-var test_x = [fitData("")];
-
-// console.log(test_x);
-predict(test_x);
+  fs.unlinkSync(tempFilePath);
+  fs.unlinkSync(tempFilePath2);
+});
+// };
